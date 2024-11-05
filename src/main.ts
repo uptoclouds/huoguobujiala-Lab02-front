4742179ee7b692e6f4bf02c44d232d68981965a4
fsr <14387260+fsr144@user.noreply.gitee.com>
2024-11-05 11:47:44 +0800

    54

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ import router from './router'
 import 'nprogress/nprogress.css'
 
 const app = createApp(App)
-
+import { inject } from '@vercel/analytics'
+inject()
 app.use(createPinia())
 app.use(router)
 
